1a5c4b2784f88f12ff106b5b02fe44edbc7828d0
Vitor Queiroz <vitoralencar881@gmail.com>
2026-07-17 23:59:00 -0300

    atualiza pesquisas do 'painel'

--- arrastao/src/app/pages/painel/painel.ts
+++ arrastao/src/app/pages/painel/painel.ts
@@ -1,10 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-painel',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './painel.html',
   styleUrl: './painel.css',
 })
-export class Painel {}
+export class Painel {
+
+  pesquisa = '';
+
+  funcionarios = [
+    {
+      nome: 'Maria Silva',
+      cpf: '12345678900',
+      setor: 'Administrativo'
+    },
+    {
+      nome: 'Carlos Souza',
+      cpf: '98765432100',
+      setor: 'Financeiro'
+    },
+    {
+      nome: 'Ana Paula',
+      cpf: '45612378900',
+      setor: 'RH'
+    },
+    {
+      nome: 'Pedro Henrique',
+      cpf: '74185296300',
+      setor: 'Financeiro'
+    }
+  ];
+
+  funcionariosFiltrados() {
+
+    return this.funcionarios.filter(funcionario =>
+      funcionario.nome.toLowerCase().includes(this.pesquisa.toLowerCase()) ||
+      funcionario.cpf.includes(this.pesquisa) ||
+      funcionario.setor.toLowerCase().includes(this.pesquisa.toLowerCase())
+
+    );
+
+  }
+}
